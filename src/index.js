04d6cfafb0f616f8c13dd6d977ece8f960593dcd
Karan Nehra <karanehra@karans-MacBook-Air.local>
2019-08-27 00:05:03 +0530

    Wrote parsing utils and database record creations

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import "babel-polyfill";
 import express from "express";
-import { databaseEmitter } from './emitters/index';
-import { INITIALIZE_DATABASE, SYNC_DATABASE } from './constants/events';
-import { Article, Log } from "./database/index";
-
+import { databaseEmitter } from "./emitters/index";
+import { INITIALIZE_DATABASE, SYNC_DATABASE } from "./constants/events";
+import { Article, Log, Feed } from "./database/index";
+import { parseFeed } from "./util/parsers";
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  Article.findAll().then(articles => {
-    res.send(articles)
-  })
-})
+global.task_queue = [];
 
-app.get("/logs", (req, res) => {
-  Log.findAll().then(logs => {
-    res.send(logs)
-  })
-})
+app.get("/", (req, res) => {
+  res.send("Service Online");
+});
+
+app.get("/logs", async (req, res) => {
+  try {
+    let logs = await Log.findAll({});
+    res.send(logs).status(200);
+  } catch (e) {
+    res.send("error").status(500);
+  }
+});
+
+app.post("/feed", async (req, res) => {
+  let feed_processed = await parseFeed(req.body.url);
+  feed_processed
+    ? res.send("Added").status(201)
+    : res.send("not added").status(400);
+});
+
+app.get("/feed", async (req, res) => {
+  let feeds = await Feed.findAll({});
+  res.send(feeds).status(200);
+});
+
+app.get("/articles", async (req, res) => {
+  let articles = await Article.findAll({});
+  res.send(articles).status(200);
+});
 
 app.listen(port, () => {
   databaseEmitter.emit(INITIALIZE_DATABASE);
