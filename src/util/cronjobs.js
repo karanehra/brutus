e5337fe5835f8889bc18590d8c53f6b0971aa6a4
Karan Nehra <karanehra@gmail.com>
2019-08-27 22:15:56 +0530

    Changed update feed cron duration

--- src/util/cronjobs.js
+++ src/util/cronjobs.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { updateFeeds } from "./parsers";
 
-cron.schedule("0 */3 * * * *", () =>{
+cron.schedule("0 */10 * * * *", () =>{
   console.log("Running feed Update cron")
   updateFeeds();
 })
