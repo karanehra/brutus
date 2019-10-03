f337e8ad9bd1cbd2daf1cf5091e0b73cca1e6cf2
Karan Nehra <karanehra@gmail.com>
2019-10-03 23:56:38 +0530

    Added basic mongo client

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express"
-import { databaseEmitter } from "./emitters/index";
-import { INITIALIZE_DATABASE, SYNC_DATABASE } from "./constants/events"
-import cors from "cors"
-import bodyParser from "body-parser"
-import apis from "./api/index"
+import express from "express";
+// import { databaseEmitter } from "./emitters/index";
+// import { INITIALIZE_DATABASE, SYNC_DATABASE } from "./constants/events"
+import cors from "cors";
+import bodyParser from "body-parser";
+import apis from "./api/index";
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,7 +12,27 @@ const port = process.env.PORT || 3000;
 
 app.use("/", apis);
 
-app.listen(port, () => {
-  databaseEmitter.emit(INITIALIZE_DATABASE);
-  databaseEmitter.emit(SYNC_DATABASE);
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = "mongodb://localhost:27017";
+
+const dbName = "myproject";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  app.listen(port, () => {
+    // databaseEmitter.emit(INITIALIZE_DATABASE);
+    // databaseEmitter.emit(SYNC_DATABASE);
+    console.log("APp up");
+  });
+
+  client.close();
 });
