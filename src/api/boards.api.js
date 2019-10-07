0681f6afa044cce48eaabb74a6c41abf4744d48c
Karan Nehra <karanehra@gmail.com>
2019-10-07 23:35:07 +0530

    Fixed Board APIs

--- src/api/boards.api.js
+++ src/api/boards.api.js
@@ -1,17 +1,13 @@
 import express from "express";
-import { Board } from "../database/index";
 import authenticationMiddleware from "../configs/authMiddleware";
+import Board from "../models/board";
 
 let router = express.Router();
-router.use(authenticationMiddleware);
+// router.use(authenticationMiddleware);
 
 router.get("/:id", async (req, res) => {
   try {
-    let board = await Board.findAll({
-      where: {
-        userId: req.params.id
-      }
-    });
+    let board = await Board.find().where({ userId: req.params.id });
     res.status(200).send(board);
   } catch (e) {
     res.status(500).send(e);
@@ -20,10 +16,8 @@ router.get("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    await Board.destroy({
-      where: {
-        id: req.params.id
-      }
+    await Board.deleteOne().where({
+      _id: req.params.id
     });
     res.status(200).send("ok");
   } catch (e) {
@@ -32,9 +26,9 @@ router.delete("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { name, userId } = req.body;
+  const { title, userId } = req.body;
   try {
-    let board = await Board.create({ name, userId });
+    let board = await Board.create({ title, userId });
     res.status(201).send(board);
   } catch (e) {
     res.status(500).send(e);
