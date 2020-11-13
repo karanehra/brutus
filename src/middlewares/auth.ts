5d4a1ffdc254cf3431445dcbf809a0e59635ce01
Karan Nehra <karanehra@gmail.com>
2020-11-13 22:42:46 +0530

    Completed auth mw

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,24 @@
 import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
 
 const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log('COMING from auth Mw')
+  const { authorization } = req.headers
+  if (!authorization) {
+    res.status(401).send({ message: 'Auth Token missing' })
+  }
+  const token = authorization.split(' ')[1]
+
+  if (token) {
+    try {
+      const { exp, ...claims } = jwt.verify(token, process.env.JWT_SECRET) as any
+    } catch (e) {
+      if (e instanceof jwt.TokenExpiredError) {
+        return res.status(401).send({ message: 'Auth Token expired' })
+      } else {
+        return res.status(401).send({ message: 'Auth Token invalid' })
+      }
+    }
+  }
   next()
 }
 
