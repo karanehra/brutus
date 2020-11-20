2c7b5995ffb1064dec99e2cdf2100479b5445310
Karan Nehra <karanehra@gmail.com>
2020-11-20 19:56:24 +0530

    Encryption middleware working fine

--- src/middlewares/encrypt.ts
+++ src/middlewares/encrypt.ts
@@ -2,17 +2,15 @@ import { Request, Response, NextFunction } from 'express'
 import crypto from 'crypto'
 
 const encryptionMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  let old = res.json
-  res.json = data => {
-    console.log(data)
-    return old.call(data)
-  }
   let key = process.env.AES_KEY
   let iv = crypto.randomBytes(16)
   let cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
-  let encrypted = cipher.update(req.url, 'utf8', 'hex')
-  encrypted += cipher.final('hex')
-  console.log(key, encrypted)
+  let old = res.json
+  res.json = function (data) {
+    let encrypted = cipher.update(JSON.stringify(data), 'utf8', 'hex')
+    encrypted += cipher.final('hex')
+    return old.call(this, { data: encrypted, iv: iv.toString('hex') })
+  }
   next()
 }
 
